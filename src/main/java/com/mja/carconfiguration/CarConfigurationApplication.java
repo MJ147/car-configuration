beef9a7de638b153824c389f8fe193438f43abbf
Marek Jaczewski <jaczewski147@gmail.com>
2020-01-30 19:08:57 +0100

    Initial comit

---
+++ src/main/java/com/mja/carconfiguration/CarConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.mja.carconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarConfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mja/carconfiguration/CarConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mja.carconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
